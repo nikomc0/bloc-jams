1369dfc1304b1580508717c8ef1ed21b74f0281d
Dominick <dominick@theosfp.com>
2017-02-12 17:01:05 -0800

    Changed animations and offset each points animation delay.

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,31 +2,23 @@ var animatePoints = function() {
 
 var points = document.getElementsByClassName('point');
 
-var revealFirstPoint = function() {
-   points[0].style.opacity = 1;
-   points[0].style.transform = "scaleX(1) translateY(0)";
-   points[0].style.msTransform = "scaleX(1) translateY(0)";
-   points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+var revealPoint = function() {
+
+   for(var i = 0; i < points.length; i++){
+     
+     if(points[i] == points[1]){
+       points[i].style.transitionDelay = ".4s";
+     } else if (points[i] == points[2]){
+       points[i].style.transitionDelay = ".6s"
+     }
+    
+     points[i].style.opacity = 1;
+     points[i].style.transform = "rotate(0) scaleX(1) translateY(0)";
+     points[i].style.msTransform = "rotate(0) scaleX(1) translateY(0)";
+     points[i].style.WebkitTransform = "rotate(0) scaleX(1) translateY(0)";
+     
+   }
+ };
+
+  revealPoint();
 };
-
-var revealSecondPoint = function() {
-   points[1].style.opacity = 1;
-   points[1].style.transform = "scaleX(1) translateY(0)";
-   points[1].style.msTransform = "scaleX(1) translateY(0)";
-   points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-var revealThirdPoint = function() {
-   points[2].style.opacity = 1;
-   points[2].style.transform = "scaleX(1) translateY(0)";
-   points[2].style.msTransform = "scaleX(1) translateY(0)";
-   points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-revealFirstPoint();
-revealSecondPoint();
-revealThirdPoint();
-
-};
-
-//animatePoints();
