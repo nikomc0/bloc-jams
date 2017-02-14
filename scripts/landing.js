f6f04a2921a37b67c0ee9443eadb2281f9e32cd8
Dominick <dominick@theosfp.com>
2017-02-13 21:43:04 -0800

    Checkpoint-9-Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,32 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+  
+  var revealPoint = function(index){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
 
-var revealFirstPoint = function() {
-   points[0].style.opacity = 1;
-   points[0].style.transform = "scaleX(1) translateY(0)";
-   points[0].style.msTransform = "scaleX(1) translateY(0)";
-   points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+  for (var i = 0; i < points.length; i++){
+    revealPoint(i);
+  }
 };
 
-var revealSecondPoint = function() {
-   points[1].style.opacity = 1;
-   points[1].style.transform = "scaleX(1) translateY(0)";
-   points[1].style.msTransform = "scaleX(1) translateY(0)";
-   points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-};
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
 
-var revealThirdPoint = function() {
-   points[2].style.opacity = 1;
-   points[2].style.transform = "scaleX(1) translateY(0)";
-   points[2].style.msTransform = "scaleX(1) translateY(0)";
-   points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-};
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
 
-revealFirstPoint();
-revealSecondPoint();
-revealThirdPoint();
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-};
-
-animatePoints();  
-
-
+  window.addEventListener('scroll', function(event) {
+    
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);   
+    }
+  });
+}
