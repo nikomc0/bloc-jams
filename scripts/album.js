c53859c0d4b7bbf9d11e3ecd39ef0d05c4dd2042
Dominick <dominick@theosfp.com>
2017-02-16 12:00:51 -0800

    Checkpoint-11 Revised for formatting.

--- scripts/album.js
+++ scripts/album.js
@@ -59,10 +59,10 @@ var setCurrentAlbum = function(album) {
 	albumSongList.innerHTML = '';
 
 	for (var i = 0; i < album.songs.length; i++) {
-	 albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
 
 window.onload = function() {
- setCurrentAlbum(albumPicasso);
+	setCurrentAlbum(albumPicasso);
 };
