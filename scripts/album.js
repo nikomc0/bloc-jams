797a61de12baa70221a2af373ff4d3a210e6fdc6
Dominick <dominick@theosfp.com>
2017-02-19 14:08:25 -0800

    Checkpoint-12

--- scripts/album.js
+++ scripts/album.js
@@ -33,9 +33,9 @@ var createSongRow = function(songNumber, songName, songLength) {
 	var template = 
 		
 		  '<tr class="album-view-song-item">'
-		+ '	<td class="song-item-number">' + songNumber + '</td>'
+		+ '	<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
 		+ '	<td class="song-item-title">' + songName + '</td>'
-		+ ' <td class="song-item-duration>' + songLength + '</td>'
+		+ ' <td class="song-item-duration">' + songLength + '</td>'
 		+ '</tr>'
 		;
 	
@@ -44,7 +44,6 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 
 var setCurrentAlbum = function(album) {
-
 	var albumTitle = document.getElementsByClassName('album-view-title')[0];
 	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -63,6 +62,30 @@ var setCurrentAlbum = function(album) {
 	}
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
+	
+	songListContainer.addEventListener('mouseover', function(event) {
+		console.log(event.target);
+		
+		// Only target individual song rows during event delegation.
+		if (event.target.parentElement.className === 'album-view-song-item') {
+			//change the content from the number to the play button.
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+	});
+	
+	for (var i = 0; i < songRows.length; i++) {
+		songRows[i].addEventListener('mouseleave', function(event) {
+			// Revert the content back to the number
+			// Selects first childe element, which is the song number.
+			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+		});
+	}
 };
